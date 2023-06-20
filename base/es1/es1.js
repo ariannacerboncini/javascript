acda35aff96749a2d48708b52902ab14ed9e5359
ariannacerboncini <116767931+ariannacerboncini@users.noreply.github.com>
2023-06-20 14:28:24 +0200

    Delete base directory

--- base/es1/es1.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById("btn"); // btn è un oggetto HTML element (HTMLButtonElement)
-const testo = document.getElementById("testo"); // testo è un oggetto HTML element (HTMLParagraphElement)
-
-btn.addEventListener("click", function() {
-  testo.textContent = "sapevate che in termini di"; // textContent è una proprietà dell'oggetto HTML element
-});
